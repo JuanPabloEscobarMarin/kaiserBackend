7e41997977ebfe2fa87a9ea0553f2314afd50108
Kobar <juescobarm@unal.edu.co>
2026-02-17 23:09:24 -0500

    refactor: adminVerify

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,16 +1,19 @@
 import { jwtVerify } from "jose";
 
 export const adminVerify = async (req, res, next) => {
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
   const token = req.cookies.jwt_token;
 
   if (!token) return res.status(401).send("Unauthorized");
 
   try {
     const { payload } = await jwtVerify(token, secret);
+    if (payload.role !== "admin") {
+      return res.status(403).send("no eres admin crack");
+    }
     next();
   } catch (error) {
     console.log(error);
     return res.status(401).send("Unauthorized");
   }
-}
+};
