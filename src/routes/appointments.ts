65b5bde2093cac461ba4ef320a99477a90451be0
Kobar <juescobarm@unal.edu.co>
2026-03-02 20:36:07 -0500

    refactor

--- src/routes/appointments.ts
+++ src/routes/appointments.ts
@@ -1,9 +1,5 @@
 import AppointmentsController from "../controllers/appointments.ts";
 import { Router } from "express";
-import {
-  cancelAppointment,
-  finishAppointment,
-} from "../controllers/appointments.ts";
 
 const router = Router();
 
@@ -11,8 +7,8 @@ router.get("/", AppointmentsController.getAll);
 router.get("/:id", AppointmentsController.getById);
 router.post("/", AppointmentsController.save);
 router.put("/:id", AppointmentsController.update);
-router.put("/:id/cancel", cancelAppointment);
-router.put("/:id/finish", finishAppointment);
+router.put("/:id/cancel", AppointmentsController.cancel);
+router.put("/:id/finish", AppointmentsController.finish);
 router.delete("/:id", AppointmentsController.delete);
 
 export default router;
