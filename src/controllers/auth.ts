1ba2c9f818408c641dc01203b71b07a13aed7eed
Kevin Jay Herrera <kjayh@unal.edu.co>
2026-03-18 19:37:27 -0500

    feature: creacion de endpoint para validacion de tokens

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,25 +4,31 @@ import userRepository from "../repositories/user.repository.ts";
 import { SignJWT } from "jose";
 
 export const login = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    const user = await userRepository.byUsername(username);
-    if (!user) return res.status(401).json({ error: "Credenciales inválidas" });
+  const { username, password } = req.body;
+  const user = await userRepository.byUsername(username);
+  if (!user) return res.status(401).json({ error: "Credenciales inválidas" });
 
-    const valid = password === user.password;
-    if (!valid) {
-        return res.status(401).json({ error: "Credenciales inválidas" });
-    }
+  const valid = password === user.password;
+  if (!valid) {
+    return res.status(401).json({ error: "Credenciales inválidas" });
+  }
 
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-    const jwt = await new SignJWT({ id: user.id, role: user.role })
-        .setProtectedHeader({ alg: "HS256" })
-        .setIssuedAt()
-        .setExpirationTime("2h")
-        .sign(secret);
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  const jwt = await new SignJWT({ id: user.id, role: user.role })
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("2h")
+    .sign(secret);
 
-    res.cookie("jwt_token", jwt).json({ message: "Login exitoso" });
+  res.cookie("jwt_token", jwt).json({ message: "Login exitoso" });
 };
 
 export const logout = (_: Request, res: Response) => {
-    res.clearCookie("jwt_token").json({ message: "Sesion terminada" });
+  res.clearCookie("jwt_token").json({ message: "Sesion terminada" });
+};
+
+export const check = (req: Request, res: Response) => {
+  res
+    .cookie("jwt_token", req.cookies["jwt_token"])
+    .json({ message: "Sesion refrescada correctamente" });
 };
